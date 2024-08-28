cb387f7cd8ff7aa70db683e111926cb8db0bb593
immark007 <immark5507@gmail.com>
2024-08-28 17:32:51 -0300

    mudanças pontuais na navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,31 @@
+'use client';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHamburger } from '@fortawesome/free-solid-svg-icons';
 export default function Navbar(){
+    const [boxShadow, setBoxShadow] = useState('none');
+
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const offset = window.scrollY;
+            if (offset > 0) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return(
-        <header className='header'>
+        <header className={`header ${scrolled ? 'scrolled' : ''}`}>
             <nav className='navigation'>
                 <a href="#" className='logo'>
                     <FontAwesomeIcon className='iconHamburguer' icon={faHamburger}/> 
